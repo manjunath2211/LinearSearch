659df080d0cb1dd511ac2722f5a765077c673111
shyam7e <60668259+shyam7e@users.noreply.github.com>
2023-10-21 14:33:49 +0530

    Create LinearSearch.c

---
+++ LinearSearch.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int linear_search(int arr[], int size, int target) {
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == target) {
+            return i; // Element found, return its index
+        }
+    }
+    return -1; // Element not found
+}
+
+int main() {
+    int arr[] = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int target = 12;
+
+    int result = linear_search(arr, size, target);
+
+    if (result != -1) {
+        printf("Element %d found at index %d\n", target, result);
+    } else {
+        printf("Element %d not found in the array\n", target);
+    }
+
+    return 0;
+}
